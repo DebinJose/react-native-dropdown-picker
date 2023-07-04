d4bac37f77a585f7219a6dd3cd7da85e510136c0
Debin Jose <89507889+DebinJose@users.noreply.github.com>
2023-07-04 17:03:16 +0530

    Update index.js
    changed the structure

--- src/translations/index.js
+++ src/translations/index.js
@@ -1,62 +1,63 @@
 export default {
-    EN: {
-        PLACEHOLDER: 'Select an item',
-        SEARCH_PLACEHOLDER: 'Select Options',
-        SELECTED_ITEMS_COUNT_TEXT: 'Select Options',
-        NOTHING_TO_SHOW: 'Select Options'
-    },
-    AR: {
-        PLACEHOLDER: 'حدد عنصر',
-        SEARCH_PLACEHOLDER:  'حدد عنصر'
-        SELECTED_ITEMS_COUNT_TEXT:  'حدد عنصر'
-        NOTHING_TO_SHOW:  'حدد عنصر'
-    },
-    FA: {
-        PLACEHOLDER: 'آیتمی انتخاب کنید',
-        SEARCH_PLACEHOLDER: 'آیتمی انتخاب کنید',
-        SELECTED_ITEMS_COUNT_TEXT: 'آیتمی انتخاب کنید',
-        NOTHING_TO_SHOW:'آیتمی انتخاب کنید',
-    },
-    TR: {
-        PLACEHOLDER: 'Bir seçenek seçin',
-        SEARCH_PLACEHOLDER: 'Bir seçenek seçin',
-        SELECTED_ITEMS_COUNT_TEXT: 'Bir seçenek seçin',
-        NOTHING_TO_SHOW: 'Bir seçenek seçin'
-    },
-    RU: {
-        PLACEHOLDER: 'Выберите элемент',
-        SEARCH_PLACEHOLDER: 'Выберите элемент',
-        SELECTED_ITEMS_COUNT_TEXT: 'Выберите элемент',
-        NOTHING_TO_SHOW: 'Выберите элемент'
-    },
-    ES: {
-        PLACEHOLDER: 'Selecciona un elemento',
-        SEARCH_PLACEHOLDER: 'Selecciona un elemento',
-        SELECTED_ITEMS_COUNT_TEXT: 'Selecciona un elemento',
-        NOTHING_TO_SHOW: 'Selecciona un elemento'
-    },
-    ID: {
-        PLACEHOLDER: 'Pilih item',
-        SEARCH_PLACEHOLDER: 'Pilih item',
-        SELECTED_ITEMS_COUNT_TEXT: 'Pilih item',
-        NOTHING_TO_SHOW: 'Pilih item'
-    },
-    IT: {
-        PLACEHOLDER: 'Seleziona un elemento',
-        SEARCH_PLACEHOLDER: 'Seleziona un elemento',
-        SELECTED_ITEMS_COUNT_TEXT: 'Seleziona un elemento'
-        NOTHING_TO_SHOW: 'Seleziona un elemento'
-    },
-    PT: {
-        PLACEHOLDER: 'Selecione um item',
-        SEARCH_PLACEHOLDER: 'Selecione um item',
-        SELECTED_ITEMS_COUNT_TEXT: 'Selecione um item'
-        NOTHING_TO_SHOW: 'Selecione um item'
-    },
-    FR: {
-        PLACEHOLDER: 'Sélectionnez un élément',
-        SEARCH_PLACEHOLDER: 'Sélectionnez un élément',
-        SELECTED_ITEMS_COUNT_TEXT: 'Sélectionnez un élément'
-        NOTHING_TO_SHOW: 'Sélectionnez un élément'
-    },
-}
+  EN: {
+    PLACEHOLDER: 'Select an item',
+    SEARCH_PLACEHOLDER: 'Select Options',
+    SELECTED_ITEMS_COUNT_TEXT: 'Select Options',
+    NOTHING_TO_SHOW: 'Select Options',
+  },
+  AR: {
+    PLACEHOLDER: 'حدد عنصر',
+    SEARCH_PLACEHOLDER: 'حدد عنصر',
+    SELECTED_ITEMS_COUNT_TEXT: 'حدد عنصر',
+    NOTHING_TO_SHOW: 'حدد عنصر',
+  },
+  FA: {
+    PLACEHOLDER: 'آیتمی انتخاب کنید',
+    SEARCH_PLACEHOLDER: 'آیتمی انتخاب کنید',
+    SELECTED_ITEMS_COUNT_TEXT: 'آیتمی انتخاب کنید',
+    NOTHING_TO_SHOW: 'آیتمی انتخاب کنید',
+  },
+  TR: {
+    PLACEHOLDER: 'Bir seçenek seçin',
+    SEARCH_PLACEHOLDER: 'Bir seçenek seçin',
+    SELECTED_ITEMS_COUNT_TEXT: 'Bir seçenek seçin',
+    NOTHING_TO_SHOW: 'Bir seçenek seçin',
+  },
+  RU: {
+    PLACEHOLDER: 'Выберите элемент',
+    SEARCH_PLACEHOLDER: 'Выберите элемент',
+    SELECTED_ITEMS_COUNT_TEXT: 'Выберите элемент',
+    NOTHING_TO_SHOW: 'Выберите элемент'
+  },
+  ES: {
+    PLACEHOLDER: 'Selecciona un elemento',
+    SEARCH_PLACEHOLDER: 'Selecciona un elemento',
+    SELECTED_ITEMS_COUNT_TEXT: 'Selecciona un elemento',
+    NOTHING_TO_SHOW: 'Selecciona un elemento'
+  },
+  ID: {
+    PLACEHOLDER: 'Pilih item',
+    SEARCH_PLACEHOLDER: 'Pilih item',
+    SELECTED_ITEMS_COUNT_TEXT: 'Pilih item',
+    NOTHING_TO_SHOW: 'Pilih item'
+  },
+  IT: {
+    PLACEHOLDER: 'Seleziona un elemento',
+    SEARCH_PLACEHOLDER: 'Seleziona un elemento',
+    SELECTED_ITEMS_COUNT_TEXT: 'Seleziona un elemento',
+    NOTHING_TO_SHOW: 'Seleziona un elemento'
+  },
+  PT: {
+    PLACEHOLDER: 'Selecione um item',
+    SEARCH_PLACEHOLDER: 'Selecione um item',
+    SELECTED_ITEMS_COUNT_TEXT: 'Selecione um item',
+    NOTHING_TO_SHOW: 'Selecione um item'
+  },
+  FR: {
+    PLACEHOLDER: 'Sélectionnez un élément',
+    SEARCH_PLACEHOLDER: 'Sélectionnez un élément',
+    SELECTED_ITEMS_COUNT_TEXT: 'Sélectionnez un élément',
+    NOTHING_TO_SHOW: 'Sélectionnez un élément'
+  },
+};
+
